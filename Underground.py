fe118e6677497b190cac5bdbf26925f9285f973a
themikkoz <mikko.jyrkas@hotmail.com>
2020-09-25 21:33:34 +0300

    Progress

--- Underground.py
+++ Underground.py
@@ -1,16 +1,27 @@
+from os import environ
+#Remove 'Hello from the pygame community. https://www.pygame.org/contribute.html' text from console
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame as pg
 
 #Logger
 def Debug(Value):
+    data = ""
+    with open("logs/debug.txt") as debugFile:
+        data = debugFile.read()
     with open("logs/debug.txt", 'w') as debugFile:
-        debugFile.write(str(Value))
+        debugFile.write(data + "\n" + str(Value))
 
 if __name__ == "__main__":
     #Initialise game engine
     pg.init()
 
     #Window settings
-    win = pg.display.set_mode((200, 200))
+    winX = 800
+    winY = 800
+    winSize = winX, winY
+    environ['SDL_VIDEO_WINDOW_POS'] = '%i,%i' % (0,0)
+    environ['SDL_VIDEO_CENTERED'] = '0'
+    win = pg.display.set_mode(winSize)
 
     #Main loop active
     Run = True
@@ -20,24 +31,86 @@ if __name__ == "__main__":
 
     #Loading assets
     def LoadAssets():
-        Dictionary = {"1":pg.image.load("assets/01.png")}
+        Dictionary = {
+            "1":pg.image.load("assets/01.png"),
+            "exit0":pg.image.load("assets/exitX00.png"),
+            "exit1":pg.image.load("assets/exitX01.png")       
+            }
         return Dictionary
     try:
         Asset = LoadAssets()
     except:
         Debug("Couldn't load assets.")
 
+    exitHover = False
+
+    #>| Background |<
+    posX = 0
+    posY = 0
+    def backgroundCheck(var):
+        if var == "both":
+            if posX <= 32:
+                posX += 1
+            else: 
+                posX = 0
+            if posY <= 32:
+                posY += 1
+            else: 
+                posY = 0
+        elif var == "y":
+            if posY <= 32:
+                posY += 1
+            else: 
+                posY = 0
+        elif var == "x":
+            if posX <= 32:
+                posX += 1
+            else: 
+                posX = 0
+        else:
+            if posX <= 32:
+                pass
+            else: 
+                posX = 0
+            if posY <= 32:
+                pass
+            else: 
+                posY = 0
+
     #Drawing method
     def Draw():
-        win.fill(0)
-        win.blit(Asset["1"], (0,0))
-        pg.display.flip()
+        #Background
+        for i in range(int(winX / 32 + 2)):
+            for ii in range(int(winY / 32 + 2)):
+                win.blit(Asset["1"], ((-32 + posX + 32 * i),(-32 + posY + 32 * ii)))
+        
+        if exitHover == True:
+            win.blit(Asset["exit1"], (winX - 32 ,0))
+        else:
+            win.blit(Asset["exit0"], (winX - 32 ,0))
+
+        #Update display
+        pg.display.update()
         
     #Main loop
     while Run:
+        #Mouse position
+        mousePos = pg.mouse.get_pos()
+        #Mouse button
+        mouseButton = pg.mouse.get_pressed()
+
+        #Event handler
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 Run = False
+            if mousePos[0] > winX - 30 and mousePos[0] < winX and mousePos[1] > 0 and mousePos[1] < 30:
+                exitHover = True
+                if mouseButton[0] != 0:
+                    Run = False
+            else:
+                exitHover = False
+            
+            
         Draw()
         Clock.tick(60)
     
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # Code name Underground
+<h5> I have no idea yet!
+
